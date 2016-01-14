10d410a014e55976fe7cbd6ff3c24d0223bff105
Lorenzo Stoakes <lstoakes@gmail.com>
2016-01-14 23:09:33 +0000

    Use more disk space by default.

--- kerndev-defaults.sh
+++ kerndev-defaults.sh
@@ -8,5 +8,5 @@ export mount_dir=$images_dir/disk
 export image_filename=rootfs_2.4.22.img
 export image_path=$images_dir/$image_filename
 export mbr_path=/usr/lib/syslinux/bios/mbr.bin
-export image_size_mb=128
+export image_size_mb=2048
 export qemu_mem=4G
