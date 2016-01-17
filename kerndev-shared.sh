62168516999d8468f4f82141522ac28305266d93
Lorenzo Stoakes <lstoakes@gmail.com>
2016-01-17 00:30:27 +0000

    Declare functions first so defaults can use them.

--- kerndev-shared.sh
+++ kerndev-shared.sh
@@ -4,5 +4,5 @@ set -e; set -o pipefail
 # See http://stackoverflow.com/a/4774063.
 script_dir=${script_dir:-(cd $(dirname $0); pwd -P)}
 
-source $script_dir/kerndev-defaults.sh
 source $script_dir/kerndev-functions.sh
+source $script_dir/kerndev-defaults.sh
