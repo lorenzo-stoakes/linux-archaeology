4790e8a075b8ab8489bd660a16efdaebab7ba73e
Lorenzo Stoakes <lstoakes@gmail.com>
2016-01-17 00:36:24 +0000

    Add initial foundations for specifying arch.

--- kerndev-defaults.sh
+++ kerndev-defaults.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 set -e; set -o pipefail
 
+# Default to host arch.
+export arch=${KERNDEV_ARCH:-$(uname -m)}
+case $arch in
+i386)
+	export musl_flags="CFLAGS=-m32"
+	export arch_patches="build32"
+	export busybox_arch=i686
+	;;
+"x86_64")
+	export busybox_arch=x86_64
+	;;
+*)
+	fatal "Unknown architecture $arch."
+	;;
+esac
+
 export max_part_path=/sys/module/loop/parameters/max_part
-export bzImage_rel_path=arch/x86_64/boot/bzImage
 export images_dir=$HOME/kerndev-images
 export mount_dir=$images_dir/disk
 export image_filename=rootfs_2.4.22.img
@@ -10,3 +25,4 @@ export image_path=$images_dir/$image_filename
 export mbr_path=/usr/lib/syslinux/bios/mbr.bin
 export image_size_mb=2048
 export qemu_mem=4G
+export bzImage_rel_path=arch/$arch/boot/bzImage
